fe14e5dfa38b628370fce980f7011c3a2e82792a
randvlover-lab <r.and.v.lover@gmail.com>
2026-09-17 16:56:14 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="gohan-otoomo-v1";
+const CACHE="gohan-otoomo-v2";
 const ASSETS=["./","./index.html","./manifest.webmanifest"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
