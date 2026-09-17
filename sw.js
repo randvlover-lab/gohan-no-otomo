7e01384e144e24518274e89b84c8ee4fbb772dbe
randvlover-lab <r.and.v.lover@gmail.com>
2026-09-17 20:45:47 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,6 @@
-const CACHE_NAME = "gohan-no-otomo-v12";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
-  );
+const CACHE_NAME = "gohan-no-otomo-v14";
+self.addEventListener("install", e => self.skipWaiting());
+self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch", e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
